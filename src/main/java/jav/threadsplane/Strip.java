ca32ffb60d3ac8406b046fd26a84d7637dee6b49
Kardinal <mowork@rambler.ru>
2019-08-20 14:54:08 +0300

    add strip blockqueue

--- src/main/java/jav/threadsplane/Strip.java
+++ src/main/java/jav/threadsplane/Strip.java
@@ -4,5 +4,28 @@ import java.util.concurrent.BlockingQueue;
 
 public class Strip implements Runnable {
 
-   
+    private final BlockingQueue<Integer> queue;
+
+    public void run() {
+
+        try {
+            while (true) {
+                Integer take = queue.take();
+                process(take);
+            }
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+
+    }
+
+    private void process(Integer take) throws InterruptedException {
+        System.out.println("The plane took off : " + take);
+        Thread.sleep(1000);
+        System.out.println("The strip is open " );
+    }
+
+    public Strip(BlockingQueue<Integer> queue) {
+        this.queue = queue;
+    }
 }
