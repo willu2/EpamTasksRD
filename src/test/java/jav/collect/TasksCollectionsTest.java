fe715786547ea65e55f01cea21c59113dfdb0921
Kardinal <mowork@rambler.ru>
2019-08-23 19:23:05 +0300

    add test class mod1

--- src/test/java/jav/collect/TasksCollectionsTest.java
+++ src/test/java/jav/collect/TasksCollectionsTest.java
@@ -3,6 +3,10 @@ package jav.collect;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.net.URISyntaxException;
+
 import static org.testng.Assert.*;
 
 public class TasksCollectionsTest {
@@ -16,4 +20,5 @@ public class TasksCollectionsTest {
         int loadNumber =  tasksCollections.reverceStack(123456);
         Assert.assertEquals(loadNumber, checkNumber);
     }
+
 }
